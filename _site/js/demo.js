e1aa9f789335197903a5dafd3cfc6d13d530ce66
honghong <285918472@qq.com>
2017-10-27 15:43:34 +0800

    加载js

---
+++ _site/js/demo.js
@@ -0,0 +1,11 @@
+// <script type="text/javascript">
+function mouseOver()
+{
+document.b1.src ="/testBlog/img/2.png"
+}
+function mouseOut()
+{
+document.b1.src ="/testBlog/img/1.png"
+}
+
+// </script>
